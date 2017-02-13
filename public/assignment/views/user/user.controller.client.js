892edcc6e5088fd55ba5383314f556767e0a4a9b
Breezen <x.wang1029@gmail.com>
2017-02-13 17:45:10 -0500

    create controllers + bind to routes views

---
+++ public/assignment/views/user/user.controller.client.js
@@ -0,0 +1,20 @@
+(function() {
+    angular
+        .module("WebAppMaker")
+        .controller("LoginController", LoginController)
+        .controller("RegisterController", RegisterController)
+        .controller("ProfileController", ProfileController);
+
+    function LoginController() {
+
+    }
+    
+    function RegisterController() {
+        
+    }
+    
+    function ProfileController() {
+        
+    }
+
+})();
