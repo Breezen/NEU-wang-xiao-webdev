6ecfb0d9c7cd4252da00fa4906f1d242577e0017
Breezen <x.wang1029@gmail.com>
2017-02-13 22:50:29 -0500

    page event handler

--- public/assignment/views/page/page.controller.client.js
+++ public/assignment/views/page/page.controller.client.js
@@ -7,6 +7,7 @@
 
     function PageListController($routeParams, PageService) {
         var vm = this;
+        vm.userId = $routeParams.uid;
         vm.websiteId = $routeParams.wid;
         function init() {
             vm.pages = PageService.findPageByWebsiteId(vm.websiteId);
@@ -14,17 +15,25 @@
         init();
     }
 
-    function NewPageController($routeParams, PageService) {
+    function NewPageController($routeParams, $location, PageService) {
         var vm = this;
+        vm.userId = $routeParams.uid;
         vm.websiteId = $routeParams.wid;
         function init() {
             vm.pages = PageService.findPageByWebsiteId(vm.websiteId);
         }
         init();
+        
+        vm.createPage = function (page) {
+            PageService.createPage(vm.websiteId, page);
+            alert("Page created!");
+            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page");
+        }
     }
 
-    function EditPageController($routeParams, PageService) {
+    function EditPageController($routeParams, $location, PageService) {
         var vm = this;
+        vm.userId = $routeParams.uid;
         vm.websiteId = $routeParams.wid;
         vm.pageId = $routeParams.pid;
         function init() {
@@ -32,6 +41,18 @@
             vm.page = PageService.findPageById(vm.pageId);
         }
         init();
+
+        vm.updatePage = function (page) {
+            PageService.updatePage(vm.pageId, page);
+            alert("Page updated!");
+            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page");
+        }
+        
+        vm.deletePage = function () {
+            PageService.deletePage(vm.pageId);
+            alert("Page deleted!");
+            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page");
+        }
     }
 
 })();
