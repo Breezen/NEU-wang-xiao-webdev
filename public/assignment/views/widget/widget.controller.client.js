99fa58eefd539172166f31edb2972e538e057024
Breezen <x.wang1029@gmail.com>
2017-02-14 01:36:40 -0500

    widget new

--- public/assignment/views/widget/widget.controller.client.js
+++ public/assignment/views/widget/widget.controller.client.js
@@ -28,12 +28,19 @@
         }
     }
 
-    function NewWidgetController($routeParams, WidgetService) {
+    function NewWidgetController($routeParams, $location, WidgetService) {
         var vm = this;
         vm.userId = $routeParams.uid;
         vm.websiteId = $routeParams.wid;
         vm.pageId = $routeParams.pid;
         vm.widgetId = $routeParams.wgid;
+        vm.widgetTypes = ["HEADER", "HTML", "IMAGE", "YOUTUBE"];
+
+        vm.createWidget = function () {
+            WidgetService.createWidget(vm.pageId, vm.widget);
+            alert("Widget created!");
+            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page/" + vm.pageId + "/widget");
+        }
     }
 
     function EditWidgetController($routeParams, $location, WidgetService) {
