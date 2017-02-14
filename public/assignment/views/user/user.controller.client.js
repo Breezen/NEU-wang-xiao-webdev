2c0d2b49436ea0438d8f06ca73cd7eb0e4236764
Breezen <x.wang1029@gmail.com>
2017-02-13 21:37:47 -0500

    user event handler

--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -5,21 +5,42 @@
         .controller("RegisterController", RegisterController)
         .controller("ProfileController", ProfileController);
 
-    function LoginController() {
+    function LoginController($location, UserService) {
         var vm = this;
+        vm.login = function (user) {
+            user = UserService.findUserByCredentials(user.username, user.password);
+            if(user) {
+                $location.url("/user/" + user._id);
+            } else {
+                alert("Unable to login");
+            }
+        }
     }
     
-    function RegisterController() {
+    function RegisterController($location, UserService) {
         var vm = this;
+        vm.register = function (user) {
+            if (user.password != user.passwordVerified) {
+                alert("Passwords different!");
+            } else {
+                user = UserService.createUser(user);
+                $location.url("/user/" + user._id);
+            }
+        }
     }
     
     function ProfileController($routeParams, UserService) {
         var vm = this;
-        var userId = $routeParams.uid;
+        vm.userId = $routeParams.uid;
         function init() {
-            vm.user = UserService.findUserById(userId);
+            vm.user = UserService.findUserById(vm.userId);
         }
         init();
+
+        vm.update = function (user) {
+            UserService.updateUser(vm.userId, user);
+            alert("Profile updated!");
+        }
     }
 
 })();
