e24a4169d774ce36a92522bbb8d69afaa29e48f0
Breezen <x.wang1029@gmail.com>
2017-02-14 00:57:02 -0500

    widget list + editor

--- public/assignment/views/widget/widget.controller.client.js
+++ public/assignment/views/widget/widget.controller.client.js
@@ -5,16 +5,64 @@
         .controller("NewWidgetController", NewWidgetController)
         .controller("EditWidgetController", EditWidgetController);
 
-    function WidgetListController() {
+    function WidgetListController($routeParams, $sce, WidgetService) {
         var vm = this;
+        vm.userId = $routeParams.uid;
+        vm.websiteId = $routeParams.wid;
+        vm.pageId = $routeParams.pid;
+        vm.widgetId = $routeParams.wgid;
+        function init() {
+            vm.widgets = WidgetService.findWidgetsByPageId(vm.pageId);
+        }
+        init();
+
+        vm.getTrustedHtml = function (html) {
+            return $sce.trustAsHtml(html);
+        }
+
+        vm.getYouTubeEmbedUrl = function (widgetUrl) {
+            var urlParts = widgetUrl.split('/');
+            var id = urlParts[urlParts.length - 1];
+            var url = "https://www.youtube.com/embed/"+id;
+            return $sce.trustAsResourceUrl(url);
+        }
     }
 
-    function NewWidgetController() {
+    function NewWidgetController($routeParams, WidgetService) {
         var vm = this;
+        vm.userId = $routeParams.uid;
+        vm.websiteId = $routeParams.wid;
+        vm.pageId = $routeParams.pid;
+        vm.widgetId = $routeParams.wgid;
     }
 
-    function EditWidgetController() {
+    function EditWidgetController($routeParams, $location, WidgetService) {
         var vm = this;
+        vm.userId = $routeParams.uid;
+        vm.websiteId = $routeParams.wid;
+        vm.pageId = $routeParams.pid;
+        vm.widgetId = $routeParams.wgid;
+
+        function init() {
+            vm.widget = WidgetService.findWidgetById(vm.widgetId);
+        }
+        init();
+
+        vm.getEditorTemplateUrl = function (type) {
+            return 'views/widget/editor/widget-' + type + '-editor.view.client.html';
+        }
+        
+        vm.updateWidget = function () {
+            WidgetService.updateWidget(vm.widgetId, vm.widget);
+            alert("Widget updated!");
+            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page/" + vm.pageId + "/widget");
+        }
+
+        vm.deleteWidget = function () {
+            WidgetService.deleteWidget(vm.widgetId);
+            alert("Widget deleted!");
+            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page/" + vm.pageId + "/widget");
+        }
     }
 
 })();
