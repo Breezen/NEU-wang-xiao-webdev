e022f1c4190d42d72e9eae51aea6bea94c2b7e5f
Breezen <x.wang1029@gmail.com>
2017-04-09 21:18:55 -0400

    project in separate repo

--- public/project/controllers/homepage.controller.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    angular
-        .module("TipTag")
-        .controller("HomepageController", HomepageController);
-    
-    function HomepageController($location) {
-
-    }
-})();
